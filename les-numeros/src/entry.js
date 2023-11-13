3c934f2152a47861c02286cca9b56f811eb8c37b
Luqmaan <lyur0001@student.monash.edu>
2023-11-13 17:35:28 +1100

    Basic gameplay added

--- les-numeros/src/entry.js
+++ les-numeros/src/entry.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Entry() {
+function Entry({ numberToMatch, onCorrectInput }) {
+  const handleInputChange = (event) => {
+    const inputData = event.target.value;
 
-    const handleInputChange = (event) => {
-        const inputData = event.target.value;
-        console.log(inputData);
-      };
-    
-    return <input className="entry-field" onChange={handleInputChange} />
+    if (parseInt(inputData, 10) === numberToMatch) {
+      console.log('correct');
+      // Call the onCorrectInput function to update the number
+      onCorrectInput();
+      //clear the entry field
+        event.target.value = '';
+    }
+    console.log(inputData);
+  };
+
+  return <input className="entry-field" onChange={handleInputChange} type={'number'} />;
 }
 
 export default Entry;
-
